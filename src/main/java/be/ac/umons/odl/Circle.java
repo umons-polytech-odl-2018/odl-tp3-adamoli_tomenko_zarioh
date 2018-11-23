5a189c8289bb943fbac8d92b697e79a0d725dae5
PTom8 <171567@umons.ac.be>
2018-11-23 10:37:34 +0100

    Diégo aime la paela

--- src/main/java/be/ac/umons/odl/Circle.java
+++ src/main/java/be/ac/umons/odl/Circle.java
@@ -4,8 +4,8 @@ public class Circle extends Ellipsis{
 	private Point upLeft;
 	private int radius;
 
-	public Circle (Point start, Point end) {
-		super(start, new Point());
+	public Circle (Point start, int radius, Point end) {
+		super(start, new Point(start.getX() + radius, start.getY() + radius));
 		this.upLeft=new Point(Math.min(start.getX(),end.getX()),Math.min(start.getY(),end.getY()));
 		this.radius = Math.min(start.getX()-end.getX(),start.getY()-end.getY());
 	}

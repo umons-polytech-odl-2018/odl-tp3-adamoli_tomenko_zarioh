707ad80857eaa235055b2858efb5f96c89411e66
PTom8 <171567@umons.ac.be>
2018-11-23 10:00:24 +0100

    Diego aime la paela

--- src/main/java/be/ac/umons/odl/Circle.java
+++ src/main/java/be/ac/umons/odl/Circle.java
@@ -1,4 +1,12 @@
 package be.ac.umons.odl;
 
-public class Circle {
+public class Circle extends Ellipsis{
+	private Point center;
+	private int radius;
+
+	public Circle (Point center, int radius) {
+		super(center, radius, radius);
+		this.center = center;
+		this.radius = radius;
+	}
 }
--- src/main/java/be/ac/umons/odl/Ellipsis.java
+++ src/main/java/be/ac/umons/odl/Ellipsis.java
@@ -1,4 +1,29 @@
 package be.ac.umons.odl;
 
-public class Ellipsis {
+import java.awt.*;
+
+public class Ellipsis implements Drawable {
+
+	private Point center;
+	private int xAxisLength;
+	private int yAxisLength;
+
+	public Ellipsis(Point O, int x, int y) {
+		this.yAxisLength=y;
+		this.xAxisLength=x;
+		this.center=O;
+	}
+
+	public double perimeter(){
+		return 2*Math.PI*Math.sqrt((Math.pow(xAxisLength,2)+Math.pow(yAxisLength,2))/2);
+	}
+
+	public double area(){
+		return Math.PI*xAxisLength*yAxisLength;
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		g.drawOval(10, 10, xAxisLength, yAxisLength);
+	}
 }
